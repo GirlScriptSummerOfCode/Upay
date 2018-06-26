2651e7b8a702e8791a72677f77d383c00ccf8f08
Divjot Singh <bogas04@gmail.com>
2018-06-26 17:49:18 +0530

    Improve About page, fix lint errors (#20)
    This is to test `now` deployments.

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,3 @@
-import NavBar from '../components/NavBar';
-import links from '../constants/links';
-
 /**
  * About page
  * @returns {React.ReactElement}
@@ -8,5 +5,15 @@ import links from '../constants/links';
 export default () => (
   <main>
     <h3>About Page</h3>
+    <section>
+      It mainly aims to provide opportunities to underprivileged children and
+      bring some sunshine in those deprived lives. Ever since it's dawn, UPAY
+      Team has been working wholeheartedly in achieving this aim. Its success
+      story can be culminated by the mere fact that where children were unable
+      to do basic arithmetic calculations are now, not just producing excellent
+      academic results but also bringing laurels to these underprivileged areas.
+      The main vision of UPAY is overcome disparities in education so that every
+      child gets an opportunity to Learn, Grow and Succeed.
+    </section>
   </main>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Item from '../components/Item';
-import NavBar from '../components/NavBar';
-import links from '../constants/links';
 
 /**
  * Home page
@@ -19,29 +17,20 @@ export default class IndexPage extends React.PureComponent {
     return (
       <React.Fragment>
         <main>
-          <img src="/images/logo.png" alt="Logo of Upay NGO" />
+          <img width="100%" src="/images/logo.png" alt="Logo of Upay NGO" />
 
           <section>
             The foundation of development for every society is the education of
-            its youth. Keeping this in mind, an{' '}
+            its youth. Keeping this in mind, an
             <a
               href="https://www.upay.org.in/"
               rel="noreferrer noopener"
               target="_blank"
             >
-              NGO UPAY
-            </a>{' '}
+              {' NGO UPAY '}
+            </a>
             (Underprivileged Advancement by Youth), was established in May 2010
-            by a group of young engineers from IITs and NITs. It mainly aims to
-            provide opportunities to underprivileged children and bring some
-            sunshine in those deprived lives. Ever since it's dawn, UPAY Team
-            has been working wholeheartedly in achieving this aim. Its success
-            story can be culminated by the mere fact that where children were
-            unable to do basic arithmetic calculations are now, not just
-            producing excellent academic results but also bringing laurels to
-            these underprivileged areas. The main vision of UPAY is overcome
-            disparities in education so that every child gets an opportunity to
-            Learn, Grow and Succeed.
+            by a group of young engineers from IITs and NITs.
           </section>
 
           <h2>Items</h2>
