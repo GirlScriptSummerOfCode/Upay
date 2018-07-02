cdc6b41c9d303c6e2bc1c5f0928c5523edd39067
SakshiShreya <shreyasakshi96@gmail.com>
2018-07-02 16:27:09 +0530

    Style changes

---
+++ styleguide.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  components: 'components/**/[A-Z]*.js',
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        }
+      ]
+    }
+  }
+};
