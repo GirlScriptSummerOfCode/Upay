f77cbb26574378f73c674a7340fefe1a194b0920
Minanshu Singh <minanshu.1998@gmail.com>
2018-07-03 17:16:44 +0530

    add unit tests for <Item /> and <Navbar />

--- components/Item/__tests__/Item.test.js
+++ components/Item/__tests__/Item.test.js
@@ -4,9 +4,15 @@ import { mount } from 'enzyme';
 import Item from '../Item';
 
 describe('<Item />', () => {
+  it('is defined', () => {
+    expect(Item).toBeDefined();
+  });
   it('renders correctly', () => {
     const wrapper = mount(<Item name="Item one" id="123" />);
-
     expect(wrapper).toMatchSnapshot();
+  });
+  it('should contain minanshu', () => {
+    const wrapper = mount(<Item name="testing" />);
+    expect(wrapper.props().name).toEqual('testing');
   });
 });
---
+++ components/NavBar/__tests__/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json'; //removes unnecessary JSON from snapshot
+import Navbar from '../index';
+import links from '../../../constants/links';
+
+describe('<Navbar />', () => {
+  it('is defined', () => {
+    expect(Navbar).toBeDefined();
+  });
+  it('renders correctly', () => {
+    const wrapper = mount(<Navbar links={links} />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
