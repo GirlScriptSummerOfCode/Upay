c2e8aa92fa1ba7dddff1ef9450fa24fe4c5e5f01
megha070 <40516822+megha070@users.noreply.github.com>
2018-07-04 14:33:41 +0530

    Update cartbutton.js

--- addtocart/cartbutton.js
+++ addtocart/cartbutton.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                +          <button onClick>Add to Cart</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <button onClick>Add to Cart</button>
+      </div>
+    );
+  }
 }
 
 export default App;
