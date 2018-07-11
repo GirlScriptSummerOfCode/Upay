78558a5783f972be30bc79b479bb2cda70a9e7e9
Divjot Singh <bogas04@gmail.com>
2018-07-11 15:44:41 +0530

    Fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
     semi: ['error', 'always'],
